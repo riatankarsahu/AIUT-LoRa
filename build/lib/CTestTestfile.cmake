fb4d68a5e497977966ce9493bcecd7c2e5da22d3
Ritankar <ritankar.sahu2009@gmail.com>
2020-08-10 16:23:38 +0200

    New Commit Demodulation

--- build/lib/CTestTestfile.cmake
+++ build/lib/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test("test_AIUT" "/bin/sh" "/home/ritankar/gr-AIUT/build/lib/test_AIUT_test.sh")
+add_test(test_AIUT "/bin/sh" "/home/ritankar/gr-AIUT/build/lib/test_AIUT_test.sh")
 set_tests_properties(test_AIUT PROPERTIES  _BACKTRACE_TRIPLES "/home/ritankar/gr-AIUT/cmake/Modules/GrTest.cmake;114;add_test;/home/ritankar/gr-AIUT/lib/CMakeLists.txt;77;GR_ADD_TEST;/home/ritankar/gr-AIUT/lib/CMakeLists.txt;0;")
--- build/python/CTestTestfile.cmake
+++ build/python/CTestTestfile.cmake
@@ -4,7 +4,7 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test("qa_Lora_Decoder" "/bin/sh" "/home/ritankar/gr-AIUT/build/python/qa_Lora_Decoder_test.sh")
+add_test(qa_Lora_Decoder "/bin/sh" "/home/ritankar/gr-AIUT/build/python/qa_Lora_Decoder_test.sh")
 set_tests_properties(qa_Lora_Decoder PROPERTIES  _BACKTRACE_TRIPLES "/home/ritankar/gr-AIUT/cmake/Modules/GrTest.cmake;114;add_test;/home/ritankar/gr-AIUT/python/CMakeLists.txt;44;GR_ADD_TEST;/home/ritankar/gr-AIUT/python/CMakeLists.txt;0;")
-add_test("qa_Lora_Demodulator" "/bin/sh" "/home/ritankar/gr-AIUT/build/python/qa_Lora_Demodulator_test.sh")
+add_test(qa_Lora_Demodulator "/bin/sh" "/home/ritankar/gr-AIUT/build/python/qa_Lora_Demodulator_test.sh")
 set_tests_properties(qa_Lora_Demodulator PROPERTIES  _BACKTRACE_TRIPLES "/home/ritankar/gr-AIUT/cmake/Modules/GrTest.cmake;114;add_test;/home/ritankar/gr-AIUT/python/CMakeLists.txt;45;GR_ADD_TEST;/home/ritankar/gr-AIUT/python/CMakeLists.txt;0;")
